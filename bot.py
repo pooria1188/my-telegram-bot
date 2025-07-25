b6e713724632cf5d7e19fa08a52a9607894f936a
pooria1188 <pooriagooli@gmail.com>
2025-07-26 00:23:05 +0330

    bot.py

--- bot.py
+++ bot.py
@@ -278,7 +278,7 @@ async def search_partner(update: Update, context: ContextTypes.DEFAULT_TYPE, sea
             return
         user_data[str(user_id)]['coins'] -= GENDER_SEARCH_COST
         save_data(user_data, USERS_DB_FILE)
-        await query.answer(f"-{GENDER_SEARCH_COST} سکه 🪙")
+        await query.answer(f"-{GENDER_SEARCH_COST} سکه �")
 
     partner_id = None
     if search_type == "random":
@@ -420,3 +420,4 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
+�
